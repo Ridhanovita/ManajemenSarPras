e27235eeaf96fd453e3ed867d0bbf5c1ac70a6a7
ndi05 <andhikap877@gmail.com>
2026-04-11 16:15:15 +0700

    melakukan perubahan di dalam pengelolahan barang

--- kelolaBarang.Designer.cs
+++ kelolaBarang.Designer.cs
@@ -28,33 +28,183 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnKembali = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelNamaBarang = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtNamaBarang = new System.Windows.Forms.TextBox();
+            this.txtIdBarang = new System.Windows.Forms.TextBox();
+            this.labelIdBarang = new System.Windows.Forms.Label();
+            this.btnTambahBarang = new System.Windows.Forms.Button();
+            this.btnUpdateBarang = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnKembali
             // 
-            this.btnKembali.Location = new System.Drawing.Point(35, 368);
+            this.btnKembali.Location = new System.Drawing.Point(21, 13);
+            this.btnKembali.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnKembali.Name = "btnKembali";
-            this.btnKembali.Size = new System.Drawing.Size(95, 38);
+            this.btnKembali.Size = new System.Drawing.Size(107, 48);
             this.btnKembali.TabIndex = 0;
             this.btnKembali.Text = "Kembali";
             this.btnKembali.UseVisualStyleBackColor = true;
             this.btnKembali.Click += new System.EventHandler(this.btnKembali_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(24, 204);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(907, 321);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Stencil", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(268, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(406, 38);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "PENGELOLAHAN BARANG";
+            // 
+            // labelNamaBarang
+            // 
+            this.labelNamaBarang.AutoSize = true;
+            this.labelNamaBarang.Location = new System.Drawing.Point(20, 150);
+            this.labelNamaBarang.Name = "labelNamaBarang";
+            this.labelNamaBarang.Size = new System.Drawing.Size(128, 20);
+            this.labelNamaBarang.TabIndex = 3;
+            this.labelNamaBarang.Text = "NAMA BARANG";
+            this.labelNamaBarang.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 20);
+            this.label3.TabIndex = 4;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtNamaBarang
+            // 
+            this.txtNamaBarang.Location = new System.Drawing.Point(184, 147);
+            this.txtNamaBarang.Name = "txtNamaBarang";
+            this.txtNamaBarang.Size = new System.Drawing.Size(289, 26);
+            this.txtNamaBarang.TabIndex = 6;
+            // 
+            // txtIdBarang
+            // 
+            this.txtIdBarang.Location = new System.Drawing.Point(184, 100);
+            this.txtIdBarang.Name = "txtIdBarang";
+            this.txtIdBarang.Size = new System.Drawing.Size(289, 26);
+            this.txtIdBarang.TabIndex = 8;
+            this.txtIdBarang.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // labelIdBarang
+            // 
+            this.labelIdBarang.AutoSize = true;
+            this.labelIdBarang.Location = new System.Drawing.Point(20, 103);
+            this.labelIdBarang.Name = "labelIdBarang";
+            this.labelIdBarang.Size = new System.Drawing.Size(99, 20);
+            this.labelIdBarang.TabIndex = 7;
+            this.labelIdBarang.Text = "ID BARANG";
+            this.labelIdBarang.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // btnTambahBarang
+            // 
+            this.btnTambahBarang.BackColor = System.Drawing.Color.Lime;
+            this.btnTambahBarang.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnTambahBarang.Location = new System.Drawing.Point(503, 96);
+            this.btnTambahBarang.Name = "btnTambahBarang";
+            this.btnTambahBarang.Size = new System.Drawing.Size(136, 34);
+            this.btnTambahBarang.TabIndex = 9;
+            this.btnTambahBarang.Text = "Tambah Barang";
+            this.btnTambahBarang.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdateBarang
+            // 
+            this.btnUpdateBarang.Location = new System.Drawing.Point(503, 143);
+            this.btnUpdateBarang.Name = "btnUpdateBarang";
+            this.btnUpdateBarang.Size = new System.Drawing.Size(136, 34);
+            this.btnUpdateBarang.TabIndex = 10;
+            this.btnUpdateBarang.Text = "Update Barang";
+            this.btnUpdateBarang.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(661, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 34);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Hapus Barang";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(661, 143);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(136, 34);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Detail Barang";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // kelolaBarang
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(954, 538);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnUpdateBarang);
+            this.Controls.Add(this.btnTambahBarang);
+            this.Controls.Add(this.txtIdBarang);
+            this.Controls.Add(this.labelIdBarang);
+            this.Controls.Add(this.txtNamaBarang);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelNamaBarang);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnKembali);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "kelolaBarang";
             this.Text = "kelolaBarang";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnKembali;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelNamaBarang;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtNamaBarang;
+        private System.Windows.Forms.TextBox txtIdBarang;
+        private System.Windows.Forms.Label labelIdBarang;
+        private System.Windows.Forms.Button btnTambahBarang;
+        private System.Windows.Forms.Button btnUpdateBarang;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- kelolaBarang.cs
+++ kelolaBarang.cs
@@ -23,5 +23,20 @@ namespace ManajemenSarPras
             dashboard.Show();
             this.Hide();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
