14374d47119473ecb2d071734cbadc3467f30066
ndi05 <andhikap877@gmail.com>
2026-04-11 19:12:52 +0700

    Update Page Report dan update semua page

--- dashboardPage.Designer.cs
+++ dashboardPage.Designer.cs
@@ -30,15 +30,16 @@
         {
             this.kelolaBarang = new System.Windows.Forms.Button();
             this.permintaanBarang = new System.Windows.Forms.Button();
-            this.pengecekanRutin = new System.Windows.Forms.Button();
+            this.labelKaryawan = new System.Windows.Forms.Button();
             this.maintennce = new System.Windows.Forms.Button();
             this.report = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // kelolaBarang
             // 
-            this.kelolaBarang.Location = new System.Drawing.Point(57, 102);
+            this.kelolaBarang.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kelolaBarang.Location = new System.Drawing.Point(115, 179);
             this.kelolaBarang.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.kelolaBarang.Name = "kelolaBarang";
             this.kelolaBarang.Size = new System.Drawing.Size(180, 50);
@@ -49,7 +50,8 @@
             // 
             // permintaanBarang
             // 
-            this.permintaanBarang.Location = new System.Drawing.Point(292, 102);
+            this.permintaanBarang.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.permintaanBarang.Location = new System.Drawing.Point(115, 303);
             this.permintaanBarang.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.permintaanBarang.Name = "permintaanBarang";
             this.permintaanBarang.Size = new System.Drawing.Size(180, 50);
@@ -58,20 +60,22 @@
             this.permintaanBarang.UseVisualStyleBackColor = true;
             this.permintaanBarang.Click += new System.EventHandler(this.permintaanBarang_Click);
             // 
-            // pengecekanRutin
+            // labelKaryawan
             // 
-            this.pengecekanRutin.Location = new System.Drawing.Point(529, 102);
-            this.pengecekanRutin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.pengecekanRutin.Name = "pengecekanRutin";
-            this.pengecekanRutin.Size = new System.Drawing.Size(180, 50);
-            this.pengecekanRutin.TabIndex = 2;
-            this.pengecekanRutin.Text = "Cek Rutin";
-            this.pengecekanRutin.UseVisualStyleBackColor = true;
-            this.pengecekanRutin.Click += new System.EventHandler(this.pengecekanRutin_Click);
+            this.labelKaryawan.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelKaryawan.Location = new System.Drawing.Point(325, 179);
+            this.labelKaryawan.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.labelKaryawan.Name = "labelKaryawan";
+            this.labelKaryawan.Size = new System.Drawing.Size(180, 50);
+            this.labelKaryawan.TabIndex = 2;
+            this.labelKaryawan.Text = "Karyawan";
+            this.labelKaryawan.UseVisualStyleBackColor = true;
+            this.labelKaryawan.Click += new System.EventHandler(this.pengecekanRutin_Click);
             // 
             // maintennce
             // 
-            this.maintennce.Location = new System.Drawing.Point(158, 242);
+            this.maintennce.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maintennce.Location = new System.Drawing.Point(115, 242);
             this.maintennce.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.maintennce.Name = "maintennce";
             this.maintennce.Size = new System.Drawing.Size(180, 50);
@@ -82,7 +86,8 @@
             // 
             // report
             // 
-            this.report.Location = new System.Drawing.Point(412, 242);
+            this.report.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.report.Location = new System.Drawing.Point(325, 242);
             this.report.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.report.Name = "report";
             this.report.Size = new System.Drawing.Size(180, 50);
@@ -91,25 +96,25 @@
             this.report.UseVisualStyleBackColor = true;
             this.report.Click += new System.EventHandler(this.report_Click);
             // 
-            // textBox1
+            // label1
             // 
-            this.textBox1.Location = new System.Drawing.Point(215, 38);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(344, 26);
-            this.textBox1.TabIndex = 6;
-            this.textBox1.Text = "selamat datang admin, semangat input datanya";
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Stencil", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(609, 57);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "SELAMAT DATANG ADMIN";
             // 
             // dashboardPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(717, 441);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.report);
             this.Controls.Add(this.maintennce);
-            this.Controls.Add(this.pengecekanRutin);
+            this.Controls.Add(this.labelKaryawan);
             this.Controls.Add(this.permintaanBarang);
             this.Controls.Add(this.kelolaBarang);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
@@ -124,10 +129,10 @@
 
         private System.Windows.Forms.Button kelolaBarang;
         private System.Windows.Forms.Button permintaanBarang;
-        private System.Windows.Forms.Button pengecekanRutin;
+        private System.Windows.Forms.Button labelKaryawan;
         private System.Windows.Forms.Button maintennce;
         private System.Windows.Forms.Button report;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
